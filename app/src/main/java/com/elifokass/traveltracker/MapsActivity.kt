eec66252427889c1f61a580de23b2d1b7abaaf3c
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-07-24 13:10:33 +0300

    LatLng, adding marker, zooming the map

--- app/src/main/java/com/elifokass/traveltracker/MapsActivity.kt
+++ app/src/main/java/com/elifokass/traveltracker/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.elifokass.traveltracker
 
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -15,6 +17,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +32,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //LatLng -> latitude , longitude - enlem, boylam
+
+        //location
+        //val harput = LatLng(38.70422772656363, 39.25721712485904)
+        //marker
+        //mMap.addMarker(MarkerOptions().position(harput).title("Harput"))
+        //zoom
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(harput,15f))
+
+
+
     }
 }
